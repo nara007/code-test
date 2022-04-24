c9353d31a7b66ae1caadb9b99cbea1daceecf28e
nara007 <songye007@gmail.com>
2022-04-24 17:10:44 +0200

    feat: add frontend pagination

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import TextField from '@mui/material/TextField';
+import MuiPagination from '@mui/material/Pagination';
 import ItemList from '../item-list/ItemList';
 import useDebounce from '../../hooks/useDebounce';
 import { useGetLibrariesByNameQuery } from '../../store/slices/bowerSearch';
@@ -14,27 +15,45 @@ const SearchFilter = styled(TextField)`
     width: 100%;
 `;
 
+const Pagination = styled(MuiPagination)`
+    & .MuiPagination-ul {
+        display: flex;
+        justify-content: center;
+        margin-top: .5rem;
+    }
+`;
+
+const pageSize = 2;
+
 function Content(): JSX.Element {
     const [searchValue, setSearchValue] = useState('');
+    const [page, setPage] = useState(1);
     const [debouncedValue] = useDebounce(searchValue, 700);
     const { data, error, isLoading } = useGetLibrariesByNameQuery(debouncedValue);
+
     const onValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchValue(e.target.value);
     };
 
-    const items = data ? data.map((item) => ({
+    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+    };
+
+    const items = data ? data.slice((page - 1) * pageSize, page * pageSize).map((item) => ({
         name: item.name,
         description: item.description,
         homepage: item.homepage,
         stars: item.stars,
         owner: item.repository_url?.split('/')?.[3] ?? '',
     })) : [];
+    const pageCount = data ? Math.ceil(data.length / pageSize) : 1;
     return (
         <>
             <SearchFilter size='small' type='search' value={searchValue} onChange={onValueChange} />
             <ListContainer>
                 {isLoading ? 'is loading...' : <ItemList items={items} />}
             </ListContainer>
+            <Pagination count={pageCount} page={page} onChange={handleChange} />
         </>
     );
 }
